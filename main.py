03657aa875cf3220a44c5fe35063d0baa042d5b1
Kozlov Yury <kye@cryptosoft.ru>
2025-02-12 09:46:39 +0300

    [wordpress_parser] Рефакторинг: Обработка изображений откорректирована.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import zipfile
 from zipfile import ZipFile
 import os
 import re
+import glob
 
 # распаковываем архив
 with zipfile.ZipFile('hugo-export.zip', 'r') as zip_ref:
@@ -42,14 +43,25 @@ def remove_unnecessary(root_dir):
         if not any(word in each_file_content for word in words):
             os.unlink(full_path)
 
-def remove_line_with_phrase(directory, phrase):
+def work_on_files(directory, phrase):
     
-#   для обработки картинок (на будущее)
-#   pattern1 = r'\[<img [^>]*wp-image-'
-#    replacement1 = r'![wp-image-'
+# обработка изображений в тексте 
+    pattern_start = r'\[<img [^>]*wp-image-' # заменим начало 
+    replacement_start = r'![wp-image-'
+   
+    pattern_png = r'png.*? />\]' # окончание стоки до png
+    replacement_png = r'png)'
 
-#    pattern2 = r'\[<img [^>]*wp-image-[^>]{4}'
-#    replacement2 = r'![wp-image-'
+    pattern_png_2 = r'(wp-image-.{4}).*?(/wp-content/uploads)'
+    
+    pattern_wp_im = r'/wp-content/uploads'
+    replacement_wp_im = r'](/wordpress/wp-content/uploads'
+
+# обработка изображения в заголовке
+    pattern_feautured = r'featured_image: '
+    replacement_feautured = r'thumbnail:\n  src: "/wordpress'
+    pattern_png_3 = r'png'
+    replacement_png_3 = r'png"\n  visibility:\n    -list'
 
     
     for filename in os.listdir(directory):
@@ -63,37 +75,71 @@ def remove_line_with_phrase(directory, phrase):
                     if line.startswith("date:"): 
                         line = line[:16] + '\n'
 
-                    # Вставляем "/wordpress/"
-                    if ("featured_image: " in line and "/wordpress" not in line):
-                        before_featured = line.split("featured_image: ")[0]
-                        after_featured = line.split("featured_image: ")[1]
-                        line = f"{before_featured}featured_image: /wordpress{after_featured}"
+                    # обработка изобржения в заголовке
+                    if ("featured_image: " in line):
+                        line = re.sub(pattern_feautured, replacement_feautured, line)
+                        line = re.sub(pattern_png_3, replacement_png_3, line)
 
-                    # Вставляем нужное обрамление для картинок                              
-                                                      
-                    #    line = re.sub(pattern1, replacement1, line)
-                    
-                    #этот код работает правильно, но обрезает все, что после первой картинки
+                    # обработка изображений в тексте
                     if "[<img" in line:
-                        after_featured1 = line.split(" wp-image")[1]
-                        after_featured2 = line.split("news/wp-content/uploads")[1]              
-                        after_featured3 = after_featured1[:5] + "](/wordpress/wp-content/uploads" + after_featured2
-                        after_featured5 = after_featured3.split("\" alt=")[0] + ')'
-                        line = f"![wp-image{after_featured5}"                      
+                        line = re.sub(pattern_start, replacement_start, line)
+                        line = re.sub(pattern_png, replacement_png, line)
+                        line = re.sub(pattern_png_2, r'\1\2', line)
+                        line = re.sub(pattern_wp_im, replacement_wp_im, line)
+
+                    # копирование используемых изображений
+                    if ".png" in line:
+
                     
                     # Удаление категории
                     if phrase not in line:
                         file.write(line)
-                    
+
+# удаление ненужных изображений и каталогов
+"""
+def find_image_paths(md_directory):
+    image_paths = set()
+    for root, _, files in os.walk(md_directory):
+        for file in files:
+            if file.endswith('.md'):
+                with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
+                    content = f.read()
+                    # Ищем пути к изображениям, начинающиеся с /wp-content
+                    paths = re.findall(r'/wp-content[^\s)]+\.(?:png|jpg)', content)
+                    image_paths.update(paths)
+    print (image_paths)
+    return image_paths
+
+def delete_unused_images(wp_content_directory, used_image_paths):
+    for root, dirs, files in os.walk(wp_content_directory, topdown=False):
+        for file in files:
+            file_path = os.path.join(root, file)
+            relative_path = os.path.relpath(file_path, wp_content_directory)
+            # Преобразуем относительный путь в формат, начинающийся с /wp-content
+            formatted_path = '/' + os.path.join('wp-content', relative_path).replace('\\', '/')
+            if formatted_path not in used_image_paths:
+               # print(f"Deleting unused file: {file_path}")
+                os.remove(file_path)
+        # Удаляем пустые каталоги
+        for dir in dirs:
+            dir_path = os.path.join(root, dir)
+            if not os.listdir(dir_path):
+                #print(f"Deleting empty directory: {dir_path}")
+                #os.rmdir(dir_path)
+
+def delete_unref(md_directory, wp_content_directory):
+    used_image_paths = find_image_paths(md_directory)
+    delete_unused_images(wp_content_directory, used_image_paths)
+"""
 
 # распаковка
 directory_path = 'hugo-export'
-#convert_filename_encoding(directory_path)
+convert_filename_encoding(directory_path)
 
 #удаление ненужных заметок (оставлю задел для фильтрации)
 words = ['Для внешней публикации']
 directory_posts = 'hugo-export/posts'
-#remove_unnecessary(directory_posts)
+remove_unnecessary(directory_posts)
 
 # Удаление 
 # - категории Для внешней публикации
@@ -101,8 +147,16 @@ directory_posts = 'hugo-export/posts'
 # Добавляем wordpress перед изображением в названии
 
 phrase_to_remove = "- Для внешней публикации"
-remove_line_with_phrase(directory_posts, phrase_to_remove)
+work_on_files(directory_posts, phrase_to_remove)
 
+# Удаление неиспоьзуемых изображжений и пустых каталогов
+#images_dir = "hugo-export"
+#delete_unref(directory_posts, images_dir)
+
+
+#md_directory = "path/to/your/md/files"  # Укажите путь к каталогу с .md файлами
+#wp_content_directory = "path/to/wp-content"  # Укажите путь к каталогу /wp-content
+#main(md_directory, wp_content_directory)
 
 
 
